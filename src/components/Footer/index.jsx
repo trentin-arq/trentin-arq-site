22743b3e58ac70fbeae61b6d80b569582cd47922
felipesantosdooca <ofelipe.dev@gmail.com>
2023-01-30 18:54:39 -0300

    feat: rotas, footer e header
    - adicionada as rotas para cada página
    - criados os componentes de footer e de header

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,48 @@
+import { BsInstagram, BsWhatsapp, BsFillTelephoneFill } from 'react-icons/bs'
+import { GrMail } from 'react-icons/gr'
+import './styles.sass'
+
+export default function Footer(){
+  return(
+    <section className='footer'>
+      <div className='logo-container'>
+        <img src='../images/logo-bg.png' alt='logo' className='logo-footer' width={180}/>
+        <span className='rights-reserved'>2023 - Trentin Arquitetura © todos os direitos reservados</span>
+      </div>
+
+      <div className='footer-contacts'>
+        <span className='footer-message'>Arquitetura | Interiores | Execução</span>
+        <div className='social-links'>
+          <a   
+            href='https://www.instagram.com/trentin.arquitetura/'
+            target='_blank'
+            rel='external'
+            className='social-link'
+          >
+            <BsInstagram color='#fff' size={28}/>
+          </a>
+          <a 
+            href='https://api.whatsapp.com/send/?phone=5551997283636&text&type=phone_number&app_absent=0'
+            target='_blank' 
+            rel='external'
+            className='social-link'
+          >
+            <BsWhatsapp color='#fff' size={28}/>
+          </a>
+        </div>
+      </div>
+
+      <div className='infos-container'>
+        <span className='adress'> Rua XXX, 999 - Bairro X / Campo Bom </span>
+        <span className='phone -info'>
+          <BsFillTelephoneFill color='#fff'/>
+          (51)99999-9999
+        </span>
+        <span className='email -info'>
+          <GrMail color='#fff'/>
+          trentinarq@gmail.com
+        </span>
+      </div>
+    </section>
+  )
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LaunchPage from '../Pages/LaunchPage'
+import Home from '../Pages/Home'
+import Projects from '../Pages/Projects'
+import About from '../Pages/About'
+import Contact from '../Pages/Contact'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function RouterApp(){
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LaunchPage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/lancamento' element={<LaunchPage/>}/>
+          <Route path='/projetos' element={<Projects/>}/>
+          <Route path='/sobre' element={<About/>}/>
+          <Route path='/contato' element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   )
 }
