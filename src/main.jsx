9a3bf30fba1b7b358cc3269251f6b31b8688cd34
felipesantosdooca <ofelipe.dev@gmail.com>
2023-02-10 22:33:46 -0300

    fix: whatsapp icon
    mudando o ícone fixo do whatsapp

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         target='_blank'
         rel='external'
         >
-        <img src='../icons/whatsapp-icon.svg' alt='whatsapp-icon' width={65}/>
+        <img src='../icons/whatsapp-icon-2.svg' alt='whatsapp-icon' width={65}/>
       </a>
     </>
   </React.StrictMode>,
