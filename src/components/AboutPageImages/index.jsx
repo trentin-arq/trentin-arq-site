65af65613ab99529bb199f83d1204c97a3626fb6
felipesantosdooca <ofelipe.dev@gmail.com>
2023-02-23 21:18:28 -0300

    update imagens

--- src/components/AboutPageImages/index.jsx
+++ src/components/AboutPageImages/index.jsx
@@ -4,14 +4,14 @@ export default function AboutPageImages(){
   return(
     <div className="about-page-images">
       <div className="definitions">
-        <img src="../images/murilo.jpg" alt="murilo-trentin" loading="lazy" className="about-image"/>
+        <img src="../images/murilo.png" alt="murilo-trentin" loading="lazy" className="about-image" width={280}/>
         <h3 className="name">Murilo Trentin</h3>
         <span className="definition-text">Arquiteto Diretor/Fundador</span>
         <span className="definition-text">Diretor criativo de projetos</span>
         <span className="definition-text">Arquitetura e Urbanismo - Unisinos (2021)</span>
       </div>
       <div className="definitions">
-        <img src="../images/julia.png" alt="julia-allgayer" loading="lazy" className="about-image"/>
+        <img src="../images/julia.png" alt="julia-allgayer" loading="lazy" className="about-image" width={280}/>
         <h3 className="name">Julia Allgayer</h3>
         <span className="definition-text">Marketing e Financeiro</span>
         <span className="definition-text">Publicidade e Propaganda - Unisinos (2020)</span>
