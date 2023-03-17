cf6a442d53e4b72e8c1ea9a58391f441ca4d85cd
zeelab-tech <zeelab.ag@gmail.com>
2023-03-17 11:08:19 -0300

    hotfix: meta data home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,14 +2,21 @@ import AboutSection from '../../components/AboutSection'
 import BannersCarousel from '../../components/BannersCarousel'
 import ArchitectureSection from '../../components/ArchitectureSection'
 import InteriorsSection from '../../components/InteriorsSection'
+import { Helmet } from 'react-helmet'
 
 export default function Home(){
   return(
-    <main>
-      <BannersCarousel/>
-      <ArchitectureSection/>
-      <InteriorsSection/>
-      <AboutSection/>
-    </main>
+    <>
+      <Helmet>
+        <meta name='title' content='Trentin Arquitetura - Escritório de arquitetura contemporânea | Campo Bom'/>
+        <meta name='description' content='Projetos e execução de residências únicas e exclusivas. Arquitetura contemporânea de alto padrão. Escritório de arquitetura localizado em Campo Bom.'/>
+      </Helmet>
+      <main>
+        <BannersCarousel/>
+        <ArchitectureSection/>
+        <InteriorsSection/>
+        <AboutSection/>
+      </main>
+    </>
   )
 }
