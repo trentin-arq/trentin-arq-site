c82e92307cebcf6d8db403f63af2260b261826b8
ofelipesant <ofelipe.dev@gmail.com>
2023-05-27 18:39:34 -0300

    feat: pixel e analytics tag
    - adicionadas as tags do pixel e google analytics

--- src/components/ActionButton/index.jsx
+++ src/components/ActionButton/index.jsx
@@ -5,17 +5,26 @@ export default function ActionButton(props){
   const {buttonText, buttonLink, external} = props
   const defaultLink = import.meta.env.VITE_WHATSAPP_LINK
 
-  function handleConversion(){
+  function gAdsConversionTrack(){
     gtag('event', 'conversion', {
       'send_to': 'AW-11112632779/dyAwCO7GrpEYEMuj9bIp',
     })
+  }
+
+  function pixelConversionTrack(){
+    fbq('track', 'Conversion')
+  }
+
+  function handlerConversion(){
+    gAdsConversionTrack()
+    pixelConversionTrack()
   }
   
   return(
     <>
       {
         external ? (
-          <a href={buttonLink || defaultLink} className="button-action" target="_blank" rel="exrernal" onClick={handleConversion}>
+          <a href={buttonLink || defaultLink} className="button-action" target="_blank" rel="exrernal" onClick={handlerConversion}>
             <div>
               <span>{buttonText}</span>
               <span>{buttonText}</span>
